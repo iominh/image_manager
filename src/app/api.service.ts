a763ec825dc2f19640c7e546da8d78f4d505e268
Minh <iominh@google.com>
2023-12-02 13:11:29 -0800

    List files

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+const API_PATH = 'http://localhost:3000'
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,7 +11,7 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getSomeData() {
-    return this.http.get('/api/images');
+    return this.http.get(`${API_PATH}/list-files`);
   }
 
   // more methods for POST, PUT, DELETE...
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'image-manager';
 
   theme = 'dark-theme'; // or 'light-theme'
 
+  constructor(private apiService: ApiService) {
+
+  }
+
   toggleTheme() {
     this.theme = this.theme === 'dark-theme' ? 'light-theme' : 'dark-theme';
+  }
+
+  ngOnInit() {
+    this.apiService.getSomeData().subscribe(files => {
+      console.log(files);
+    })
   }
 }

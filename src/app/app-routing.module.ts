c5d57d59a51fd1010ba87e974b44fc4a556401b7
Minh <iominh@google.com>
2023-12-02 12:00:42 -0800

    Add material symbols

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
-  { path: '*', component: MainComponent },
+  { path: 'images', component: MainComponent },
 ];
 
 @NgModule({

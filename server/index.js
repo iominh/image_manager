433c1feaa79a081dbc904745ff55c7a2c0219e40
Minh <iominh@google.com>
2023-12-02 13:05:57 -0800

    List files

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
@@ -9,6 +12,22 @@ console.log('Serving static images from', imagePath);
 
 // Serve images as static resources
 app.use('/images', express.static(imagePath));
+
+app.get('/list-files', (req, res) => {
+    fs.readdir(imagePath, (err, files) => {
+      if (err) {
+        res.status(500).send('Error reading directory');
+        return;
+      }
+  
+      // Filter out directories and only keep files
+      let fileList = files.filter(file => {
+        return fs.statSync(path.join(imagePath, file)).isFile();
+      });
+  
+      res.json(fileList);
+    });
+  });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
